f196001e2492dc1bbb3c8d45c62f55790923f5c0
gabrielsx1805 <gabr1el_sx1805@gmail.com>
2023-08-28 17:21:15 -0300

    att

--- EX - 10/ATV6.py
+++ EX - 10/ATV6.py
@@ -2,17 +2,19 @@ a = int(input("Digite um numero: "))
 b = int(input("Digite um numero: "))
 c = int(input("Digite um numero: "))
 
-if a >= b and c:
-    print(a, ' é o maior numero')
-elif b >= a and c:
-    print(b, ' é o maior numero')
-elif c >= a and b:
-    print(c, ' é o maior numero')
+maior = a
 
-
-if a <= b and c:
-    print(a, ' é o menor numero')
-elif b <= c and a:
-    print(b, ' é o menor numero')
-elif c <= a and b:
-    print(c, ' é o menor numero')
+if b > a and b>c:
+    maior = b
+if c > a and c > b:
+    maior = c
+    
+menor = a
+if b < c and b < a:
+    menor = b
+if c < b and c < a:
+    menor = c
+    
+    
+print("O maior numero é: ",maior)
+print("O menor numero é: ",menor)
--- EX - 10/ATV8.py
+++ EX - 10/ATV8.py
@@ -2,7 +2,7 @@ a = float(input("Digite o lado A em cm: "))
 b = float(input("Digite o lado B em cm: "))
 c = float(input("Digite o lado C em cm: "))
 
-if a+b >= c and a+c >= b and c+b >= a:
+if a+b > c and a+c > b and c+b > a:
     print("é um triagulo")
 else: 
     print("não é um triangulo!")
